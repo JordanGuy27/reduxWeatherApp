1e10f58dd322d8b204480403fdfe252c4573c671
Jordan Guy <jordan.guy.277@gmail.com>
2018-09-10 14:47:13 -0400

    creating list forecast list for different cities

--- reduxWeatherApp/src/reducers/reducerWeather.js
+++ reduxWeatherApp/src/reducers/reducerWeather.js
@@ -1,3 +1,13 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+
+    switch(action.type) {
+        case FETCH_WEATHER:
+            return [action.payload.data, ...state];
+            //make a new array, put the new data inside and spread all the of previous entries into one array
+            //DO NOT WANT TO MUTATE STATE
+    }
+
     return state;
 }
