da2b5a5a0a739eb6c746c6a96233942b4c319db8
Jordan Guy <jordan.guy.277@gmail.com>
2018-09-20 12:28:28 -0400

    added pressure and humidity charts

--- reduxWeatherApp/src/containers/weatherList.js
+++ reduxWeatherApp/src/containers/weatherList.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 //always remove export default on the class once the export default connect has been written
 class WeatherList extends Component {
 
-
+    //first argument for every call will be an object containing the city data
     renderWeather(cityData) {
 
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
-        console.log(temps);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        //these variables are handling the data, therefore the sparklines component will not need to have access to Redux
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <Sparklines height={120} width={180} data={temps}>
-                        <SparklinesLine color="red" />
-                    </Sparklines>
-                </td>
+                <td><Chart data={temps} color="green" units="K" /></td>
+                <td><Chart data={pressures} color="red" units="hPa" /></td>
+                <td><Chart data={humidities} color="blue" units="%" /></td>
             </tr>
         )
     }
@@ -30,15 +30,16 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.props.weather.map(this.renderWeather)}
+                    {this.props.weather.map(this.renderWeather)} 
                 </tbody>
             </table>
+            //call function for every city object in the array
         )
     }
 }
